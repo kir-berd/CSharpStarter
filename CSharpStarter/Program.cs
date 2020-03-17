5bd2972da7fefabf9dd6cb25f7050ac1e2cf75b1
Kirill B <re1f@ya.ru>
2020-03-17 23:38:00 +0500

    Complete first lesson. I learned how to create very simple WPF application.

---
+++ CSharpStarter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpStarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
