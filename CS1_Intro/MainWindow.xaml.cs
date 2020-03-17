3c29ebbc7b7b96c33b7b6e2794d3df28043c54c9
Kirill B <re1f@ya.ru>
2020-03-17 23:52:41 +0500

    Test

---
+++ CS1_Intro/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace CS1_Intro
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик нажатия на кнопку
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+    }
+}
