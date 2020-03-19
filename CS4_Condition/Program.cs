379f7b657a94e9a2c3ad693f5b9360265420225b
Kirill B <re1f@ya.ru>
2020-03-19 20:23:30 +0500

    Completed lesson 4

---
+++ CS4_Condition/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace CS4_Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Задание 1");
+            Translator.Translate();*/
+
+            /*Console.WriteLine("Задание 2");
+            Console.Write("Введите оператор 1: ");
+            int operator1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите оператор 2: ");
+            int operator2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите знак: ");
+            string sign = Console.ReadLine();
+            int result = 0;
+            switch (sign)
+            {
+                case "+":
+                    result = operator1 + operator2;
+                    break;
+                case "-":
+                    result = operator1 - operator2;
+                    break;
+                case "*":
+                    result = operator1 * operator2;
+                    break;
+                case "/":
+                    if (operator1 == 0 || operator2 == 0)
+                    {
+                        Console.WriteLine("На ноль делить нельзя!");
+                    }
+                    else
+                    {
+                        result = operator1 / operator2;
+                    }
+                    break;
+            }
+            Console.WriteLine($"Результат: {result}");*/
+
+            Console.WriteLine("\nЗадание 3");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if(number >= 0 && number <=14)
+            {
+                Console.WriteLine("0-14");
+            }
+            else if(number >= 15 && number <= 35)
+            {
+                Console.WriteLine("15-35");
+            }
+            else if (number >= 36 && number <= 50)
+            {
+                Console.WriteLine("36-50");
+            }
+            else if (number >= 50 && number <= 100)
+            {
+                Console.WriteLine("50-100");
+            }
+            else
+            {
+                Console.WriteLine("Out of range");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CS4_Condition/Translator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS4_Condition
+{
+    class Translator
+    {
+        static public void Translate()
+        {
+            string input = Console.ReadLine();
+            string output;
+
+            switch(input)
+            {
+                case "солнечно":
+                    output = "sunny";
+                    break;
+                case "жара":
+                    output = "heat";
+                    break;
+                case "холодно":
+                    output = "cold";
+                    break;
+                case "ветер":
+                    output = "wind";
+                    break;
+                default:
+                    output = "Такого слова нет";
+                    break;
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
